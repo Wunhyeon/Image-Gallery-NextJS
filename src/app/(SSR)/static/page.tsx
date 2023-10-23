d329054d7b87a1df629c4df74f256f79166ed612
Jaehyeon <xhwogusxh@gmail.com>
2023-10-24 01:05:57 +0900

    again

--- src/app/(SSR)/static/page.tsx
+++ src/app/(SSR)/static/page.tsx
@@ -18,8 +18,8 @@ export default async function Page() {
   );
   const image: UnsplashImage = await response.json();
 
-  const width = Math.min(500, image?.width);
-  const height = (width / image?.width) * image?.height;
+  const width = Math.min(500, image.width);
+  const height = (width / image.width) * image.height;
 
   return (
     <div className="d-flex flex-column align-items-center">
@@ -29,15 +29,13 @@ export default async function Page() {
         image after refreshing the page until we compile the project again.
       </Alert>
       <Image
-        src={image?.urls.raw}
+        src={image.urls.raw}
         width={width}
         height={height}
-        alt={image?.description}
+        alt={image.description}
         className="rounded shadow mw-100 h-100"
       />
-      <Link href={"/users/" + image?.user.username}>
-        {image?.user.username}
-      </Link>
+      <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
     </div>
   );
 }
