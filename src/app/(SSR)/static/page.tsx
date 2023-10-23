696bb312886c32ad04cd432bbf406928157d5c6d
Jaehyeon <xhwogusxh@gmail.com>
2023-10-23 23:25:48 +0900

    static page 선생님껄로 변경

--- src/app/(SSR)/static/page.tsx
+++ src/app/(SSR)/static/page.tsx
@@ -1,20 +1,58 @@
+// import { UnsplashImage } from "@/models/unsplash-image";
+// import { Metadata } from "next";
+// import Image from "next/image";
+// import Link from "next/link";
+// import { Alert } from "@/components/bootstrap";
+
+// export const metadata: Metadata = {
+//   title: "Static Fetching - Image Gallery",
+//   description: "Static",
+// };
+
+// export default async function Page() {
+//   const response = await fetch(
+//     "https://api.unsplash.com/photos/random?client_id=" +
+//       process.env.UNSPLASH_ACCESS_KEY
+//     // NEXT_PUBLIC_을 앞에 붙여야만 client에서 사용할 수 있고, 보일 수 있다.
+//     // 지금은 서버에서 사용하기 때문에 클라이언트에 보이지도 않고, NEXT_PUBLIC을 붙이지 않으면 undefined로 나온다.
+//   );
+//   const image: UnsplashImage = await response.json();
+
+//   const width = Math.min(500, image.width);
+//   const height = (width / image.width) * image.height;
+
+//   return (
+//     <div className="d-flex flex-column align-items-center">
+//       <Alert>
+//         This page <strong>fetches and caches data at build time.</strong>
+//         Eventhough the Unsplash aPI always returs a new image, we see the same
+//         image after refreshing the page until we compile the project again.
+//       </Alert>
+//       <Image
+//         src={image.urls.raw}
+//         width={width}
+//         height={height}
+//         alt={image.description}
+//         className="rounded shadow mw-100 h-100"
+//       />
+//       <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
+//     </div>
+//   );
+// }
+
 import { UnsplashImage } from "@/models/unsplash-image";
-import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { Alert } from "@/components/bootstrap";
 
-export const metadata: Metadata = {
-  title: "Static Fetching - Image Gallery",
-  description: "Static",
+export const metadata = {
+  title: "Static Fetching - NextJS 13.4 Image Gallery",
 };
 
 export default async function Page() {
   const response = await fetch(
     "https://api.unsplash.com/photos/random?client_id=" +
       process.env.UNSPLASH_ACCESS_KEY
-    // NEXT_PUBLIC_을 앞에 붙여야만 client에서 사용할 수 있고, 보일 수 있다.
-    // 지금은 서버에서 사용하기 때문에 클라이언트에 보이지도 않고, NEXT_PUBLIC을 붙이지 않으면 undefined로 나온다.
   );
   const image: UnsplashImage = await response.json();
 
@@ -24,8 +62,8 @@ export default async function Page() {
   return (
     <div className="d-flex flex-column align-items-center">
       <Alert>
-        This page <strong>fetches and caches data at build time.</strong>
-        Eventhough the Unsplash aPI always returs a new image, we see the same
+        This page <strong>fetches and caches data at build time</strong>. Even
+        though the Unsplash API always returns a new image, we see the same
         image after refreshing the page until we compile the project again.
       </Alert>
       <Image
@@ -35,6 +73,7 @@ export default async function Page() {
         alt={image.description}
         className="rounded shadow mw-100 h-100"
       />
+      by{" "}
       <Link href={"/users/" + image.user.username}>{image.user.username}</Link>
     </div>
   );
